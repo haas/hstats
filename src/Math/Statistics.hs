9f13cd724dcc66e62077a0389cdd0ad37b288bd8
Khudyakov Alexey <Khudyakov Alexey alexey.skladnoy@gmail.com>
2009-07-05 23:22:08 +0400

    Loosen class restrictions from Floating to Fractioanl where possible

--- src/Math/Statistics.hs
+++ src/Math/Statistics.hs
@@ -53,15 +53,15 @@ import Data.List
 import Data.Ord (comparing)
 
 -- |Numerically stable mean
-mean :: Floating a => [a] -> a
+mean :: Fractional a => [a] -> a
 mean x = fst $ foldl' (\(!m, !n) x -> (m+(x-m)/(n+1),n+1)) (0,0) x
 
 -- |Same as 'mean' 
-average :: Floating a => [a] -> a
+average :: Fractional a => [a] -> a
 average = mean
 
 -- |Harmonic mean
-harmean :: (Floating a) => [a] -> a
+harmean :: (Fractional a) => [a] -> a
 harmean xs = fromIntegral (length xs) / (sum $ map (1/) xs)
 
 -- |Geometric mean
@@ -69,7 +69,7 @@ geomean :: (Floating a) => [a] -> a
 geomean xs = (foldr1 (*) xs)**(1 / fromIntegral (length xs))
 
 -- |Median
-median :: (Floating a, Ord a) => [a] -> a
+median :: (Fractional a, Ord a) => [a] -> a
 median x | odd n  = head  $ drop (n `div` 2) x'
          | even n = mean $ take 2 $ drop i x'
                   where i = (length x' `div` 2) - 1
@@ -100,7 +100,7 @@ range :: (Num a, Ord a) => [a] -> a
 range xs = maximum xs - minimum xs
 
 -- | Average deviation. I.e. mean of absolute value of deviation from mean.
-avgdev :: (Floating a) => [a] -> a
+avgdev :: (Fractional a) => [a] -> a
 avgdev xs = mean $ map (\x -> abs(x - m)) xs
     where
       m = mean xs
